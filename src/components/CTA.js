0da3e460f08d8fa2053090bbb55bc9ca06b03ce1
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-18 23:32:52 +0400

    Finish CTA component

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -8,7 +8,7 @@ const CTA = () => (
       <h2 className={styles.heading2}>Let’s try our service now!</h2>
       <p className={`${styles.paragraph} max-w-[550px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
     </div>
-    <div>
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
       <Button />
     </div>
   </section>
