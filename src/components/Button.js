6872cb14a2017900aa29acba3b60ba04535ec24c
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-18 22:19:29 +0400

    Finish CardDeal Section

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import styles from '../style'
 
 const Button = (styles) => {
   return (
-    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10' >Get Started</button>
+    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-xl ' >Get Started</button>
   )
 }
 
--- src/components/CardDeal.js
+++ src/components/CardDeal.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import styles, { layout } from '../style'
+import Button from "./Button.js"
+import { card } from '../assets'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section id='cardDeal' className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className='sm:block hidden' /> in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[550px] mt-5`}>
+        Arcu tortor, purus in mattis at sed integer faucibus.
+        Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+      </p>
+      <Button />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]' />
+    </div>
+
+  </section>
+
+)
 
 export default CardDeal
+
+
+// <section id='features' className={layout.section}>
+//       <div className={layout.sectionInfo}>
+//         <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we’ll handle the money. </h2>
+//         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+//           With the right credit card, you can improve your
+//           financial life by building credit, earning rewards and saving money.
+//           But with hundreds of credit cards on the market.
+//         </p>
+//         <Button />
+//       </div>
+//       <div className={`${layout.sectionImg} flex-col`}>
+//         {features.map((featuer) => (
+//           <FeatureCard
+//             key={featuer.id}
+//             {...featuer}
+//           />
+//         ))}
+//       </div>
+//     </section>
+
