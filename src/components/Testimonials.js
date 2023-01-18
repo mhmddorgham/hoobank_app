dae60e1d3c266e93862ca1a685db82e613fb9277
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-18 22:53:35 +0400

    Finish Testimonial Section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,7 +6,7 @@ import FeedbackCard from "./FeedbackCard";
 const Testimonials = () => {
   return (
     <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-      <div />
+      <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient' />
       {/* this div diplay flex */}
       <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 m-6 relative z-[1]'>
         <h2 className={styles.heading2}>
@@ -27,6 +27,7 @@ const Testimonials = () => {
           />
         ))}
       </div>
+
 
     </section>
   )
