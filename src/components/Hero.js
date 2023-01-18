7d853c0d1b4f1f7db76c9fe5729e23a27afa02e9
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-18 20:46:51 +0400

    Finish Hero Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,7 @@ import GetStarted from "./GetStarted"
 const Hero = () => {
   return (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      {/* Left side of the dive */}
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         {/* 20% Discount part: */}
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 ">
@@ -20,13 +21,33 @@ const Hero = () => {
             The Next <br className='sm:block hidden' /> {" "}
             <span className='text-gradient'>Generation</span> {" "}
             <br />
-            Payment Method
           </h1>
           <div className='ss:flex hidden md:mr-4 mr-0'>
             <GetStarted />
           </div>
         </div>
+
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+          Payment Method
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
+          We examine annual percentage rates, annual fees.
+        </p>
       </div>
+
+      {/* Right side of the side */}
+      <div className={` flex flex-1 ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="robot" className="w-[100%] h-[100%] relative z-[5]" />
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
+        <div className='absolute z-[2] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+
     </section>
   )
 }
