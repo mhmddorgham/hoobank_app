3aff540b26a5abd8bb68cfa72338307d219ad371
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-18 23:16:00 +0400

    Finish Cleint Components

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from '../style';
+import Button from './Button';
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className='flex flex-1 flex-col'>
+      <h2 className={styles.heading2}>Let’s try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[550px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    </div>
+    <div>
+      <Button />
+    </div>
+  </section>
+)
 
 export default CTA
