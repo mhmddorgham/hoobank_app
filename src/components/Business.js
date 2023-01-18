eb4638e7d6a544a22063cfd3adea28bb0c5172e5
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-18 21:10:07 +0400

    Finish Button Components

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { features } from '../constants'
+import styles, { layout } from '../style'
+import Button from "./Button.js"
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we’ll handle the money. </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your
+          financial life by building credit, earning rewards and saving money.
+          But with hundreds of credit cards on the market.
+        </p>
+        <Button />
+      </div>
+    </section>
   )
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from '../style'
 
-const Button = () => {
+const Button = (styles) => {
   return (
-    <div>Button</div>
+    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10' >Get Started</button>
   )
 }
 
